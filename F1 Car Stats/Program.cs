87b86ab26e6948ffb74c5a7d0a547e1aa2542572
PanosMoutarelos <panosmout.pm@gmail.com>
2025-11-24 17:46:46 +0200

    Initial commit with F1 Car Stats project

---
+++ F1 Car Stats/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace F1_Car_Stats
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stats formulaSeat1 = new Stats("Mercedes Petronas AMG", 1000, 786.0, 2010, "Brackley, England", "1.6 liter V6 turbo hybrid engine" );
+            Stats formulaSeat2 = new Stats("Scuderia Ferrari", 1000, 798.0, 1950, "Maranello, Italy", "1.6-liter V6 turbo hybrid engine");
+
+            Console.WriteLine("===== Formula1 Car Stats =====\n" );
+
+            formulaSeat1.StatsSummary();
+
+           formulaSeat2.StatsSummary();
+
+            formulaSeat1.ComparePowerToWeight(formulaSeat2);
+
+            Console.ReadKey();  
+        }
+    }
+}
---
+++ F1 Car Stats/Stats.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F1_Car_Stats
+{
+    internal class Stats
+    {
+        private string _teamName = "";
+        private int _horsePower = 0;
+        private double _weight = 0;
+        private int _year = 0;
+        private string _constructor = "";
+        private string _engineType = "";
+
+        public string TeamName { get => _teamName; set => _teamName = value; }
+        public int HorsePower
+        {
+            get => _horsePower;
+
+            set
+            {
+                if (value <= 0)
+                {
+
+                    Console.WriteLine("Need a number bigger than that");
+
+                }
+
+                else
+                {
+                    _horsePower = value;
+                }
+            }
+        }
+
+        public double Weight
+        {
+            get => _weight;
+
+
+            set
+            {
+
+                if (value <= 0)
+                {
+                    Console.WriteLine("Weight must be a positive number");
+                }
+                else
+
+                {
+                    _weight = value;
+                }
+
+            }
+        }
+
+        public int Year { get => _year; set => _year = value; }
+        public string Constructor { get => _constructor; set => _constructor = value; }
+        public string EngineType { get => _engineType; set => _engineType = value; }
+
+        public Stats(string teamName, int horsePower, double weight, int year, string constructor, string engineType)
+        {
+            TeamName = teamName;
+            HorsePower = horsePower;
+            Weight = weight;
+            Year = year;
+            Constructor = constructor;
+            EngineType = engineType;
+        }
+
+        public double PowerToWeight()
+        {
+            double PTW = HorsePower / Weight;
+            return PTW;
+        }
+
+
+        public void ComparePowerToWeight(Stats otherCar)
+        {
+
+            if (this.PowerToWeight() > otherCar.PowerToWeight())
+
+            {
+                Console.WriteLine("\n" + this.TeamName + " has bigger power to weight ratio: " + this.PowerToWeight().ToString("F3"));
+
+            }
+            else if (this.PowerToWeight() < otherCar.PowerToWeight())
+            {
+                Console.WriteLine("\n" + otherCar.TeamName + " has bigger power to weight ratio: " + otherCar.PowerToWeight().ToString("F3"));
+            }
+            else if ((this.PowerToWeight() == otherCar.PowerToWeight()))
+            {
+                Console.WriteLine("\nBoth cars have the same power to weight ratio");
+            }
+        }
+        public void StatsSummary()
+        {
+            Console.WriteLine("Team Name: " + this.TeamName);
+            Console.WriteLine("HorsePower " + this.HorsePower + " hP");
+            Console.WriteLine("Weight: " + this.Weight + "Kg" );
+            Console.WriteLine("Year of creation: " + this.Year);
+            Console.WriteLine("Location:  " + this.Constructor);
+            Console.WriteLine("Engine Type: " + this.EngineType);
+            Console.WriteLine($"Power to Weight ratio: {this.PowerToWeight().ToString("F3")} hP/kg\n");
+            
+        }
+    }
+}
